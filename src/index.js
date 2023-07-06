4c1258638ae8930be887dbc29028e088569860a1
Kacegz <90635502+Kacegz@users.noreply.github.com>
2023-07-06 22:28:51 +0200

    Add main file

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {header} from './header.js';
+import middle from './middle.js';
+import footer from './footer.js';
+import home from "./home.js";
+import menu from "./menu.js";
+import contact from './contact.js';
+const content=document.querySelector("#content");
+content.appendChild(header());
+content.appendChild(middle());
+home();
+content.appendChild(footer());
+
+(function clickable(){
+    const homebutton=document.getElementById("home");
+    const menubutton=document.getElementById("menu");
+    const contactsbutton=document.getElementById("contact");
+    homebutton.addEventListener('click',home);
+    menubutton.addEventListener('click',menu);
+    contactsbutton.addEventListener('click',contact);
+})()
